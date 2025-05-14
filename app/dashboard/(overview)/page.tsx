2e4d32b0dc057d7c5bfe709249b3be1f364db71d
Skupits <purnamacahya433@gmail.com>
2025-05-14 08:44:40 +0700

    w

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -13,11 +13,11 @@ export default async function Page() {
         {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
         {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
         {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        <Card
+        {/*<Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        />
+        />*/}
       </div>
 
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
